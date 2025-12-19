fae3dd71524b2b42e9bfb831db4067f7922c55af
rishu2506 <rishugoel25.it@gmail.com>
2025-12-19 20:41:26 +0530

    config folder is added

---
+++ config/coupons.js
@@ -0,0 +1,17 @@
+module.exports = {
+  OFR001: {
+    discount: 10,
+    distance: [0, 200],
+    weight: [70, 200]
+  },
+  OFR002: {
+    discount: 7,
+    distance: [50, 150],
+    weight: [100, 250]
+  },
+  OFR003: {
+    discount: 5,
+    distance: [50, 250],
+    weight: [10, 150]
+  }
+};
